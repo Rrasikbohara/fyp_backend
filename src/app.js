7d32b80c1d158864df0772148a0a2031b1afbbf3
Rasik Bohara <ryderrasik1@gmail.com>
2025-05-18 13:59:04 +0545

    backend-deployment-final

--- src/app.js
+++ src/app.js
@@ -54,7 +54,7 @@ app.use(morgan('combined', {
 }));
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: 'https://fyp-frontend-orpin.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
